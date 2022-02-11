b0096b4fa912f7971acee6a34bee4e98ad3273ab
papasmf1 <papasmf1@gmail.com>
2022-02-11 13:37:51 +0900

    리엑트와 노드 연동 데모

---
+++ DemoReactNode/cs-test/servers/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require('cors'); 
+const bodyParser = require('body-parser');
+const port =process.env.PORT || 3001;
+
+app.use(cors()); 
+app.use(bodyParser.json());
+app.use('/api', (req, res)=> res.json({username:'전우치'}));
+
+app.listen(port, ()=>{
+    console.log(`express is running on ${port}`);
+})
