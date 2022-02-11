1c4b1307b751343f23ae4a8bf7e93cc1a9f4b11d
papasmf1 <papasmf1@gmail.com>
2022-02-11 09:26:38 +0900

    앵귤러 수정

--- DemoAngular/study/src/app/app.component.ts
+++ DemoAngular/study/src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent {
   }
 
   public ifIwereGoToServer() : void {  //요 메소드이다.
-    //어떤 특정 데이터를 주는 서버에 갔다왔다고 하자.
+    //어떤 특정 데이터를 주는 서버에 갔다왔다고 
     //예를들어 ajax를 실행하고 나서 result값이 아래와 같은 배열이 나왔다고 가정하자.
     this.all.push('Result');
     this.all.push(12345);
